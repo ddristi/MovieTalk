909dd47f2990165ad010b77a6db04b665b1e4be1
Ddristi Biswas <ddristibiswas617@gmail.com>
2025-10-12 21:48:16 +0530

    fix: correct movie image handling in post creation and ensure proper response structure

--- backend/public/src/controllers/post.controller.js
+++ backend/public/src/controllers/post.controller.js
@@ -1,3 +1,11 @@
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import Post from "../models/post.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import asyncHandler from "../utils/asynchandler.js"
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+
 
 const createPost =  asyncHandler(async(req,res) =>{
     
@@ -9,13 +17,18 @@ const createPost =  asyncHandler(async(req,res) =>{
     
     const imageLocalPath = req.files?.image[0]?.path
 
-    const movieImage = await uploadOnCloudinary(imageLocalPath)
+    const movieImagePath = await uploadOnCloudinary(imageLocalPath)
+
+    const movieImage = {
+        url:movieImagePath.url,
+        public_id: movieImagePath.public_id
+    }
 
     const post = Post.create({
         title,
         deescription,
         rating,
-        movieImage
+        movieImage : movieImage
     })
 
     const createdPost = await Post.findById(post._id)
@@ -28,4 +41,56 @@ const createPost =  asyncHandler(async(req,res) =>{
     .status(200)
     .json( new ApiResponse(200, "Posted successfully"))
 
+})
+
+const updatePost = asyncHandler(async(req,res) =>{
+    const {description} = req.body;
+
+    if(!description) {
+      throw new ApiError(400, "No change in description")
+    }
+
+    const post = await Post.findByIdAndUpdate({
+        req.post?._id,
+        {
+            $set:{description}
+        },
+        {
+            new:true
+        }
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Post updated successfully")
+    )
+})
+
+const deletePost = asyncHandler(async(req,res) =>{
+    const post = await Post.findById(req.params._id)
+
+    if(!post){
+        throw new ApiError(404, "Post not found")
+    }
+
+    if(post.movieImage?.public_id){
+      try {
+           await cloudinary.uploader.destroy(post.movieImage.public_id) 
+        } catch (error) {
+           throw new ApiError(400, "Error deleting old image") 
+      }
+    }
+
+    const deletedPost =await Post.findByIdAndDelete(post._id)
+
+    if(!deletedPost){
+        throw new ApiError(400, "Something went wrong while deleting")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Post deleted successfully")
+    )
 })
